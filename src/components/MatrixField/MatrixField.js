44b807526cffc240fc7c0a92b27b8cbc1761b93e
Rodrigo Ligeiro <rrligeiro@gmail.com>
2021-08-31 19:06:04 -0300

    add functions

--- src/components/MatrixField/MatrixField.js
+++ src/components/MatrixField/MatrixField.js
@@ -3,16 +3,70 @@ import './matrixfield.css';
 
 export default function MatrixField(){
   const [ matrixSize, setMatrixSize ] = useState(2);
-  console.log(matrixSize);
+  const [ entryMatrix, setEntryMatrix ] = useState(true);
+  const [ matrix, setMatrix ] = useState([]);
+
+  function matrixGenerator(matrixSize) {
+    return(
+      <div>
+        <p>Entre com os valores da sua matriz</p>
+        {[...Array(matrixSize*matrixSize)].map(() => {
+          return(
+            <input type='number' />
+          )
+        })}
+        <button type="button" onClick={()=>{ saveMatrix(); setEntryMatrix(false) }}>Escalonamento</button>
+        <button type="button" onClick={()=>{ saveMatrix(); addIndentityMatrix(); setEntryMatrix(false) }}>Inversão de matriz</button>
+      </div>
+    )
+  }
+
+  function saveMatrix(){
+    let k = 0;
+    for (let i = 0; i < matrixSize; i++) {
+      let tempArray = [];
+      for (let j = 0; j < matrixSize; j++) {
+        tempArray.push(Number(document.querySelectorAll('input')[k].value));
+        k++;
+      }
+      matrix.push(tempArray);
+    }
+  }
+
+  function addIndentityMatrix(){
+    for (let i = 0; i < matrixSize; i++) {
+      for (let j = matrixSize; j < matrixSize*2; j++) {
+        matrix[i][j] = 0;
+        if ( i == j - matrixSize) {
+          matrix[i][j] = 1;
+        }
+      }
+    }
+  }
+  
+  function matrixOperations(){
+    console.log(matrix);
+
+    return(
+      <div>
+        {matrix.map((element) => {
+          return(
+            <div>{element}</div>
+            )
+          })}
+        <p>Operações:</p>
+      </div>
+    )
+  }
 
   return(
     <div>
-    <button type="button" onClick={() => setMatrixSize(2)}>2x2</button>
-    <button type="button" onClick={() => setMatrixSize(3)}>3x3</button>
-    <button type="button" onClick={() => setMatrixSize(4)}>4x4</button>
-    <button type="button" onClick={() => setMatrixSize(5)}>5x5</button>
-    <button type="button" onClick={() => setMatrixSize(6)}>6x6</button>
-    {matrixSize}
+    <button type="button" onClick={() => {setMatrix([]); setMatrixSize(2); setEntryMatrix(true)}}>2x2</button>
+    <button type="button" onClick={() => {setMatrix([]); setMatrixSize(3); setEntryMatrix(true)}}>3x3</button>
+    <button type="button" onClick={() => {setMatrix([]); setMatrixSize(4); setEntryMatrix(true)}}>4x4</button>
+    <button type="button" onClick={() => {setMatrix([]); setMatrixSize(5); setEntryMatrix(true)}}>5x5</button>
+    <button type="button" onClick={() => {setMatrix([]); setMatrixSize(6); setEntryMatrix(true)}}>6x6</button>
+    {entryMatrix ? matrixGenerator(matrixSize) : matrixOperations()}
     </div>
   );
 }
