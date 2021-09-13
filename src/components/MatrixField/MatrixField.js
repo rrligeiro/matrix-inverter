94ce94f8b23d257269920733d45b6cde47f61664
Rodrigo Ligeiro <rrligeiro@gmail.com>
2021-09-13 20:17:27 -0300

    Add matrix operations without number fields

--- src/components/MatrixField/MatrixField.js
+++ src/components/MatrixField/MatrixField.js
@@ -45,11 +45,27 @@ export default function MatrixField(){
   }
   
   function mutiplyByScalar(scalar, line){
-    for (let i = 0; i < matrixSize; i++) {
+    for (let i = 0; i < matrix[line].length; i++) {
       matrix[line][i] = matrix[line][i]*scalar
     }
     setMatrix(matrix.map((e)=>{return e.map((e)=>{return e})}));
-    console.log(matrix);
+    console.log(matrix)
+  }
+
+  function changeLines(line1, line2){
+    let tempArray = matrix[line1];
+    matrix[line1] = matrix[line2];
+    matrix[line2] = tempArray;
+    setMatrix(matrix.map((e)=>{return e.map((e)=>{return e})}));
+    console.log(matrix)
+  }
+
+  function multiplyAndSumLines(scalar, line1, line2){
+    for (let i = 0; i < matrix[line1].length; i++) {
+      matrix[line2][i] = matrix[line2][i] + (matrix[line1][i]*scalar);
+    }
+    setMatrix(matrix.map((e)=>{return e.map((e)=>{return e})}));
+    console.log(matrix)
   }
 
   function matrixDisplay(){
@@ -57,7 +73,9 @@ export default function MatrixField(){
       <div>
         {matrix.map((e) => {return(e.map((e)=> {return(<div>{e}</div>)}))})}
         <p>Operações:</p>
-        <button type="button" onClick={()=>{mutiplyByScalar(3, 1)}}>Multiplicar linha por escalar</button>
+        <button type="button" onClick={()=>{mutiplyByScalar(2, 1)}}>Multiplicar linha por escalar</button>
+        <button type="button" onClick={()=>{changeLines(0, 1)}}>Trocar linhas</button>
+        <button type="button" onClick={()=>{multiplyAndSumLines(-2, 1, 0)}}>Multiplicar linha e somar a outra</button>
       </div>
     )
   }
